a3f3f9d59bc712d8c5118a52fa632076c08cbbeb
Shayan Karimi <shayankarimi0078@gmail.com>
2022-03-26 10:58:04 +0430

    Add custom metrics

---
+++ products/metrics.py
@@ -0,0 +1,3 @@
+from prometheus_client import Counter
+
+api_products_created = Counter('api_products_created', 'Total products created')
--- products/viewsets.py
+++ products/viewsets.py
@@ -2,6 +2,12 @@ from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
 
+from .metrics import api_products_created
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def create(self, request, *args, **kwargs):
+        api_products_created.inc()
+        return super().create(request, *args, **kwargs)
